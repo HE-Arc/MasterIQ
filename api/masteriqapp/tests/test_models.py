ceefc0c4a0e4f5268f7c6f734b5c97826db18f7c
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-03 10:08:59 +0100

    add IQManager

--- api/masteriqapp/tests/test_models.py
+++ api/masteriqapp/tests/test_models.py
@@ -34,3 +34,17 @@ class ModelTestCases(TestCase):
         assert len(Option.objects.all()) >= 2
         assert len(User.objects.filter(username="test")) == 1
         assert len(IQ.objects.filter(user=user_test, category=category_test)) == 1
+
+    def test_use_manager(self):
+        category_test = Category.objects.create(name="test_managers")
+        user_test = User.objects.create_user("test_managers", "test_managers@example.com", "password")
+        iq_test = IQ.objects.create(user=user_test, category=category_test, iq = 102)
+
+        leaderboard = IQ.objects.get_best_players_of_category(category=category_test)
+        iq_cat = IQ.objects.get_iq_of_user_in_category(user=user_test, category=category_test)
+        all_iq = IQ.objects.get_all_iq_of_user(user=user_test)
+        assert len(leaderboard) >= 1
+        assert iq_cat == 102
+        assert len(all_iq) == 1
+        assert all_iq[0]["iq"] == 102
+        assert all_iq[0]["category"] == category_test.id
