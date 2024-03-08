8a6e888d03db2c84a05e7a5e27df04b063881a32
Plumey Simon <simon.plumey@he-arc.ch>
2024-03-08 11:26:10 +0100

    Creating customInput component

---
+++ frontend/src/components/CustomInput.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, computed, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    hasLabel: {
+        type: Boolean,
+        default: true,
+        required: false,
+    },
+    value: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'text',
+        required: false,
+    },
+    required: {
+        type: Boolean,
+        default: false,
+        required: false,
+    },
+});
+
+const name = computed(() => props.label.toLowerCase());
+
+const emit = defineEmits(['input', 'change']);
+
+// Peut ajouter la validation ici
+const onInput = (event) => {
+    emit('input', event.target.value);
+};
+
+const onChange = (event) => {
+    emit('change', event.target.value);
+};
+</script>
+
+<template>
+    <div class="input-container">
+        <label v-if="hasLabel" :for="name">{{ label }}<span v-if="props.required">*</span></label>
+        <input :type="type" :id="name" :name="name" :value="value" @input="onInput" @change="onChange" maxlength="250" :required="required"/>
+    </div>
+</template>
+
+<style scoped>
+input 
+{
+    display: block; 
+    width: 100%;
+    padding: 0.5rem 1rem;
+    border-radius: 2rem;
+    border-color: var(--color-border);
+    border-style: solid;
+}
+</style>
