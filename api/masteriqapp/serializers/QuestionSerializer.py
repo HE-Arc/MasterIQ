7954dda2e0c9504ebb8e3f37a970bae60e7f28c8
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-14 21:24:33 +0100

    change question new output

--- api/masteriqapp/serializers/QuestionSerializer.py
+++ api/masteriqapp/serializers/QuestionSerializer.py
@@ -4,6 +4,7 @@ from django.apps import apps
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(many=False, read_only=True, slug_field='name')
     class Meta:
         model = apps.get_app_config("masteriqapp").get_model("Question")
         fields = ['id', 'text', 'category']
