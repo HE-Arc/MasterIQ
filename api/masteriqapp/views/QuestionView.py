6286d236ca375eb00735a5ff7f494668d161d7d2
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-14 21:23:05 +0100

    add new community question route

--- api/masteriqapp/views/QuestionView.py
+++ api/masteriqapp/views/QuestionView.py
@@ -10,6 +10,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
+from masteriqapp.serializers.QuestionAndOptionsSerializer import QuestionAndOptionsSerializer
 from masteriqapp.serializers.QuestionSerializer import QuestionSerializer
 
 masteriq = apps.get_app_config("masteriqapp")
@@ -19,8 +20,8 @@ class QuestionView(viewsets.ViewSet):
     question_model = masteriq.get_model("Question")
     queryset = category_model.objects.all()
 
-    @action(detail=True, methods=["GET"], url_path="new")
-    def new_question(self, request, pk):
+    @action(detail=True, methods=["GET"])
+    def new(self, request, pk):
         category = get_object_or_404(self.queryset, pk=pk)
         if 'question' in request.session:
             actual_question = self.question_model.objects.get(pk=request.session['question'])
@@ -34,3 +35,24 @@ class QuestionView(viewsets.ViewSet):
         print(request.session['question'])
         serializer = QuestionSerializer(new_question)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["POST"])
+    def new_community(self, request):
+        datas = request.data
+        if not ('question' in datas and 'answer' in datas and 'options' in datas):
+            print("bad 1")
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if len(datas['options']) < 1:
+            print("bad 2")
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        category = self.category_model.objects.get(name="Community")
+        question = self.question_model.objects.create(text=datas['question'], category=category)
+        options = []
+        for option in datas['options']:
+            options.append(masteriq.get_model('Option').objects.create(text=option, is_correct=False, question=question))
+        options.append(masteriq.get_model('Option').objects.create(text=datas['answer'], is_correct=True, question=question))
+
+        question_serializer = QuestionAndOptionsSerializer(question)
+
+        return Response(question_serializer.data, status=status.HTTP_201_CREATED)
