fc88c5ad91dbd0dada9e745616905eebb533d864
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-15 09:06:05 +0100

    add test

--- api/masteriqapp/tests/test_routes.py
+++ api/masteriqapp/tests/test_routes.py
@@ -12,3 +12,21 @@ class RouteTestCases(TestCase):
         assert response.json()["1"]["category_name"] is not None
         assert response.json()["1"]["user_iq"] is not None
         assert response.status_code == 200
+
+        response = c.get("/question/1/new/")
+        assert response.status_code == 200
+        assert response.json()['id'] is not None
+        assert response.json()['text'] is not None
+        assert response.json()['category'] is not None
+
+        response = c.post("/question/new_community/", {
+            "question": "How old is Harry Potter at the beginning of the first book?",
+            "options": ["11", "15", "He wasn\'t born"],
+            "answer": "1"})
+        print(response.status_code)
+        assert response.status_code == 201
+        assert response.json()['id'] is not None
+        assert response.json()['text'] is not None
+        assert response.json()['category'] is not None
+        assert response.json()['options'] is not None
+        assert len(response.json()['options']) >= 2
