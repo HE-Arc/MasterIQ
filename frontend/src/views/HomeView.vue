4e460ef1c54f0f8880a94e1ce303a37785ef8378
Plumey Simon <simon.plumey@he-arc.ch>
2024-03-15 21:24:45 +0100

    Adding environement variables

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
+const API_SERVER = import.meta.env.VITE_API_SERVER;
 
 const categories = ref([]);
-
+console.log(API_SERVER);
 const fetchCategories = async (query) => {
-    const response = await axios.get('http://127.0.0.1:8000/api/category/iq/', {
+    const response = await axios.get(`${API_SERVER}/api/category/iq/`, {
         params: query
     });
     categories.value = response.data;
@@ -22,7 +23,7 @@ onMounted(() => {
         <h1>This is home page with categories</h1>
         <ul>
             <li v-for="category in categories" :key="category.id">
-                {{ category.name }}
+                {{ category.category_name }}
             </li>
         </ul>
         <button class="btn">Random question</button>
