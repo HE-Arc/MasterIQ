24032056cedbc6a0ae5aac1ee0f679db634172d7
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-16 13:11:48 +0100

    add environment variable for backend

--- api/masteriq/settings.py
+++ api/masteriq/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,16 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^=xy+akjf7r600zo6r+_5fku9!1aw)hhf_&feny8r3@6ds%rds'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    os.getenv('FRONTEND_URL'),
+    os.getenv('BACKEND_URL'),
 ]
 
 # Application definition
