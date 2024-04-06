4c707d120dd9eba42615fcca92f138601af16f9c
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-04-06 11:49:07 +0200

    create iq object when creating user

--- api/masteriqapp/views/AuthenticationView.py
+++ api/masteriqapp/views/AuthenticationView.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login, logout
@@ -5,8 +6,13 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 
+import masteriqapp.models.IQ
+
+masteriq = apps.get_app_config("masteriqapp")
 
 class AuthenticationView(viewsets.ViewSet):
+    category_model = masteriq.get_model("Category")
+    iq_model = masteriq.get_model("IQ")
     @action(detail=False, methods=['POST'], permission_classes=[AllowAny])
     def login(self, request):
         username = request.data.get('username')
@@ -29,7 +35,18 @@ class AuthenticationView(viewsets.ViewSet):
         password = request.data.get('password')
         if not User.objects.filter(username=username).exists():
             user = User.objects.create_user(username=username, password=password)
+            self.create_iq_objects_for_new_user(user)
             login(request, user)
             return Response({'message': 'Register successful'}, status=status.HTTP_201_CREATED)
         else:
             return Response({'message': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def create_iq_objects_for_new_user(self, user):
+        categories = self.category_model.objects.all()
+        for category in categories:
+            try:
+                already_existing_entry = self.iq_model.objects.get_iq_of_user_in_category(user=user, category=category)
+                print(already_existing_entry)
+            except masteriqapp.models.IQ.DoesNotExist:
+                self.iq_model.objects.create(user=user, category=category, iq=100)
+
