050a9835c6a559c1acdd45e3bc7361b4b2a95eeb
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-04-21 13:12:25 +0200

    shuffle options in new questions

--- api/masteriqapp/views/QuestionView.py
+++ api/masteriqapp/views/QuestionView.py
@@ -84,23 +84,24 @@ class QuestionView(viewsets.ViewSet):
         else:
             return Response(data={"field": "Question", "errors": question_serializer.errors},
                             status=status.HTTP_400_BAD_REQUEST)
-
-        for option in datas['options']:
-            option_serializer = OptionSerializer(data={"text": option, "is_correct": False, "question":  question.id})
+        options = []
+        for i in range(len(datas['options'])):
+            if i == int(datas['answer']):
+                options.append([datas['options'][i], True])
+            else:
+                options.append([datas['options'][i], False])
+        random.shuffle(options)
+        for option in options:
+            option_serializer = OptionSerializer(data={"text": option[0], "is_correct": option[1], "question":  question.id})
 
             if option_serializer.is_valid():
                 option_serializer.save()
             else:
-                return Response(data={"field": "Option", "error": option_serializer.errors},
+                field = "Option"
+                if option[1]:
+                    field = "Answer"
+                return Response(data={"field": field, "error": option_serializer.errors},
                                 status=status.HTTP_400_BAD_REQUEST)
-
-        option_serializer = OptionSerializer(
-            data={"text": datas['answer'], "is_correct": True, "question": question.id})
-        if option_serializer.is_valid():
-            question = option_serializer.save()
-        else:
-            return Response(data={"field": "Answer", "error": option_serializer.errors},
-                            status=status.HTTP_400_BAD_REQUEST)
 
         return Response(question_serializer.data, status=status.HTTP_201_CREATED)
 
